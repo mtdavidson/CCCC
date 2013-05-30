52d2aad57ea392ac00550e422120d498581308d5
mtdavidson <mark@4each.co.uk>
2013-05-30 20:27:21 +0100

    Koolkingdoms query to function

--- cccc.py
+++ cccc.py
@@ -8,6 +8,40 @@ from BeautifulSoup import BeautifulSoup
 conn = sqlite3.connect('cards.sqlite')
 c = conn.cursor()
 
+#TODO Check cards table exists if not create it
+
+def query_koolkingdoms (card_number):
+    result = {}
+    url = 'http://www.koolkingdom.co.uk/acatalog/info_' + card_number + '.html';
+    try:
+        try:
+            #Attempt to get page with standard URL
+            page = urllib2.urlopen(url).read();
+        except urllib2.HTTPError:
+            #On failure attempt alternative
+            page = urllib2.urlopen(url.replace ("_", "-")).read()
+    except urllib2.HTTPError:
+        print card_number + " could not be found"
+        return result;
+
+    soup = BeautifulSoup(page)
+    soup.prettify()
+
+    card_name = soup.title.string
+
+    price = soup.find('actinic:prices').span
+    regex = re.compile(".*\xa3([0-9]+\.[0-9]+).*")
+    r = regex.search(str (price))
+    price = r.groups ()[0]
+
+    result = {
+        'card_number': card_number,
+        'card_name': card_name,
+        'price': price
+    }
+
+    return result;
+
 while True:
     card_number = raw_input("Please Enter a Card Number: ")
     card_number = card_number.upper();
@@ -29,39 +63,20 @@ while True:
 
     qty = int (qty_input);
 
-    url = 'http://www.koolkingdom.co.uk/acatalog/info_' + card_number + '.html';
-    try:
-        try:
-            #Attempt to get page with standard URL
-            page = urllib2.urlopen(url).read();
-        except urllib2.HTTPError:
-            #On failure attempt alternative
-            page = urllib2.urlopen(url.replace ("_", "-")).read()
-    except urllib2.HTTPError:
-        print card_number + " could not be found"
+    kk = query_koolkingdoms (card_number);
+
+    if (len (kk) == 0):
         continue;
 
-    soup = BeautifulSoup(page)
-    soup.prettify()
-
-    #TODO: Clean price
-    card_name = soup.title.string
-
-    price = soup.find('actinic:prices').span
-    print price;
-    regex = re.compile(".*\xa3([0-9]+\.[0-9]+).*")
-    r = regex.search(str (price))
-    price = r.groups ()[0];
-
     #TODO: Use string formatting
-    print card_number + " " + card_name + " " + " " + str(qty) + " " + str(float (price) * qty) + "[" + price + "]";
+    print kk['card_number'] + " " + kk['card_name'] + " " + " " + str(qty) + " " + str(float (kk['price']) * qty) + "[" + kk['price'] + "]";
 
     try:
-        c.execute ("INSERT INTO cards VALUES (?, ?, ?, ?)", (card_number, card_name, qty, price))
+        #c.execute ("INSERT INTO cards VALUES (?, ?, ?, ?)", (card_number, card_name, qty, price))
         print "Added"
     except sqlite3.IntegrityError as e:
         print "Duplicate"
-        c.execute ("UPDATE cards SET qty = qty + ?, price = ? WHERE card_number = ?", (qty, price, card_number));
+        #c.execute ("UPDATE cards SET qty = qty + ?, price = ? WHERE card_number = ?", (qty, price, card_number));
         print "QTY Incremented and price updated"
 
     conn.commit ()
